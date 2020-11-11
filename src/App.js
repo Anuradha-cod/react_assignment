e15e2fea8c231637fabe87c567763fc3edb388f5
Anuradha <anuradhabansal728989@gmail.com>
2020-11-11 21:07:42 +0530

    function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header';
-import Main from './components/main';
-import Footer from './components/footer';
+// import Header from './components/header';
+// import Main from './components/main';
+// import Footer from './components/footer';
+
+import axios from "axios";
+import React, {Component } from "react";
 
 
-function App() {
+class App extends Component{
+constructor(props){
+  super(props);
+  this.state ={
+    data : []
+  }
+}
+componentDidMount(){
+  axios
+  .get("https://jsonplaceholder.typicode.com/todos")
+.then(res => {
+  return res.data;
+})
+ .then(
+   result => {
+     this.setState({ data: result});
+   },
+   error =>{
+     this.setState({ error});
+   }
+ );
+
+}
+
+render () {
+  console.log("RENDER");
+  console.log(this.state.data);
   return ( 
-    <div className="App">
-      <Header />
-      <Main/>
-      <Footer/>
+    <div className="App my-list">
+      <h1>ToDo App</h1>
+      {this.state.data && 
+      this.state.data.map(e => {
+      return<div className={`render ${e.completed == true ? "green":"red"}`} key={e.id}>{e.title}</div>;
+      })}
     </div>
   );
 }
-
+}
 export default App;
